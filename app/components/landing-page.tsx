85579f1627835b17b2a37c14ab6116362628ab3b
Can Guden <canguden@gmail.com>
2024-10-12 00:14:44 +0200

    minimal look

--- app/components/landing-page.tsx
+++ app/components/landing-page.tsx
@@ -3,7 +3,6 @@
 import React from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
-import logo from "@/public/milkyroad-logo.png";
 import icon from "@/public/icon-milkyroad.png";
 import Link from "next/link";
 
@@ -37,20 +36,14 @@ const MilkyRoadHero = () => {
         </div>
 
         {/* Content */}
-        <div className="z-10 flex flex-col items-center justify-center h-[fit] xl:h-[fit] text-white text-center px-4 font-mono">
+        <div className="z-10 flex flex-col items-center justify-center h-[900px] text-white text-center px-4 font-mono">
           <motion.div
             className=" text-6xl md:text-8xl font-bold mb-6 text-transparent bg-clip-text bg-gradient-to-r from-blue-400 via-purple-500 to-pink-500"
             initial={{ y: -50, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             transition={{ duration: 1 }}
           >
-            <Image
-              src={logo}
-              alt="MilkyRoad Inc"
-              width={500}
-              height={500}
-              className="object-cover w-[250px] h-[250px] xl:w-[500px] xl:h-[500px]"
-            />
+            <h1>Milkyroad Inc.</h1>
           </motion.div>
           <motion.p
             className="max-w-2xl text-xl md:text-2xl mb-8"
@@ -58,10 +51,9 @@ const MilkyRoadHero = () => {
             animate={{ y: 0, opacity: 1 }}
             transition={{ delay: 0.5, duration: 1 }}
           >
-            We are a cosmic digital agency, crafting stellar websites and apps
-            that take your brand to the far reaches of the digital universe. Our
-            team of star developers and nebula designers are ready to launch
-            your project into cyberspace.
+            We are a digital agency focused on creating innovative products. One
+            of our latest projects is Snaparoid, bringing back the photo-sharing
+            experience.
           </motion.p>
           <motion.div
             className="text-lg md:text-xl"
@@ -69,7 +61,8 @@ const MilkyRoadHero = () => {
             animate={{ y: 0, opacity: 1 }}
             transition={{ delay: 1, duration: 1 }}
           >
-            <p>Ready to embark on a digital odyssey?</p>
+            <p>Feel free to contact us anytime.</p>
+
             <a
               href="mailto:contact@milkyroad.com"
               className="inline-block mt-4 px-6 py-3 bg-gradient-to-br from-blue-900 via-purple-900 to-pink-800 rounded-lg hover:from-blue-600 hover:to-purple-700 transition-all duration-300"
@@ -92,9 +85,9 @@ const MilkyRoadHero = () => {
               <div>
                 <Link
                   className="inline-block text-sm px-6 py-3 bg-gradient-to-br from-blue-900 via-purple-900 to-pink-800 rounded-lg hover:from-blue-600 hover:to-purple-700 transition-all duration-300"
-                  href="https://snapshotzz.com"
+                  href="https://Snaparoid.com"
                 >
-                  Snapshotzz.com
+                  Snaparoid →
                 </Link>
               </div>
             </div>
